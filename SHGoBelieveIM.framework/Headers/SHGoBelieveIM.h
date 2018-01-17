82dec925816942c3121efff5766ea2ab2b10af90
Icarus <15188588180@163.com>
2018-01-17 18:15:34 +0800

    添加静态库

---
+++ SHGoBelieveIM.framework/Headers/SHGoBelieveIM.h
@@ -0,0 +1,19 @@
+//
+//  SHGoBelieveIM.h
+//  SHGoBelieveIM
+//
+//  Created by angle on 2018/1/17.
+//  Copyright © 2018年 angle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHGoBelieveIM.
+FOUNDATION_EXPORT double SHGoBelieveIMVersionNumber;
+
+//! Project version string for SHGoBelieveIM.
+FOUNDATION_EXPORT const unsigned char SHGoBelieveIMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHGoBelieveIM/PublicHeader.h>
+
+
