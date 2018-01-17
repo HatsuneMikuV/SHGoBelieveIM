0d5988521fca3d7690356ae53e132b9a73334278
Icarus <15188588180@163.com>
2018-01-17 19:59:26 +0800

    update

--- SHGoBelieveIMDemo/SHGoBelieveIMDemo/AppDelegate.m
+++ SHGoBelieveIMDemo/SHGoBelieveIMDemo/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import <SHGoBelieveIM/SHGoBelieveIM.h>
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [SHIMManger sharedInstance].host = @"";
     return YES;
 }
 
@@ -30,11 +34,14 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[SHIMManger sharedInstance] enterBackground];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    [[SHIMManger sharedInstance] enterForeground];
+
 }
 
 
